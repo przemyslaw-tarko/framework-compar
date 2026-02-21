ae2208adf803ee14d1bf16cb5df5644450bd01f8
Tarkowski Przemysław <tarko.przemyslaw@gmail.com>
2026-02-21 22:37:09 +0100

    fix test na fw-cypress

--- tests/cypress/e2e/c_smoke.cy.js
+++ tests/cypress/e2e/c_smoke.cy.js
@@ -1,8 +1,9 @@
 import { isTypedArray } from "util/types";
 
+Cypress.on("uncaught:exception", () => false);
+
 describe("Cypres Smoke", () => {
   isTypedArray('check title contains "Test App"', () => {
-    Cypress.on("uncaught:exeption", () => false);
     cy.visit("/wp-login.php");
     cy.title().should("include", "Test App");
   });
